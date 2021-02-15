390d1e6b43ac0110b097aacb34ea38b8f37b9e92
aguacoder123 <52142361+aguacoder123@users.noreply.github.com>
2021-02-14 22:48:30 -0500

    footer and social icons

--- fureverfriends/src/App.test.js
+++ fureverfriends/src/App.test.js
@@ -1,5 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import firebase from "firebase";
+
+function App(){
+
+}
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ fureverfriends/src/Firebase.js
@@ -0,0 +1,37 @@
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+//
+//    // const firebaseConfig = {
+//    //      projectId: "fureverfriends-66bae",
+//    //      messagingSenderId: "1029056317935",
+//    //      appId: "1:1029056317935:web:27e3df2335d7074bbd0e1a",
+//    //      measurementId: "G-1XM6V6EB86"
+//    //    };
+//
+// // Set the configuration for your app
+// // TODO: Replace with your project's config object
+// var config = {
+//     apiKey: "AIzaSyCbirB-vp_FHR0dZXE7C9G_wBgew3WbfVg",
+//     authDomain: "fureverfriends-66bae.firebaseapp.com",
+//     // For databases not in the us-central1 location, databaseURL will be of the
+//     // form https://[databaseName].[region].firebasedatabase.app.
+//     // For example, https://your-database-123.europe-west1.firebasedatabase.app
+//     databaseURL: "https://fureverfriends-66bae-default-rtdb.firebaseio.com/",
+//     storageBucket: "fureverfriends-66bae.appspot.com",
+// };
+// firebase.initializeApp(config);
+//
+// // Get a reference to the database service
+// var database = firebase.database();
+//
+// var petname, petage, pettype, furlength, gender, personality;
+// function PetInfo(){
+//     petname = document.getElementById().value;
+//     petage = document.getElementById().value;
+//     pettype = document.getElementById().value;
+//     furlength = document.getElementById().value;
+//     gender = document.getElementById().value;
+//     personality = document.getElementById().value;
+// }
+// export default firebase;
