bcf36fef09380bf7ed1eb883b945b230325c32ff
jpa5180 <61556479+jpa5180@users.noreply.github.com>
2021-02-15 03:14:35 -0500

    AI Demo - Python

---
+++ AI Demo/logistic_reg.py
@@ -0,0 +1,81 @@
+########################################################
+#
+# CMPSC 488: FureverFriends AI Demo
+#     Stochastic GD Activation Function
+#
+########################################################
+
+
+
+student_name = 'Jean P. Astudillo Guerra'
+student_email = 'jpa5180@psu.edu'
+
+
+
+########################################################
+# Import
+########################################################
+
+import math
+import numpy as np
+from random import seed
+from random import random
+import matplotlib.pyplot as plt
+
+########################################################
+# Functions
+########################################################
+
+
+
+########################################################
+# Main function
+########################################################
+if __name__ == '__main__':
+    file = open("data.txt", "r")
+
+    data = []
+    x = []
+    y = []
+
+
+    for line in file:
+        data.append([float(num) for num in line.split()])
+
+    Beta_0 = float(input("Enter an initial Beta 0 value: "))
+    Beta_1 = float(input("Enter an initial Beta 1 value: "))
+    alpha = float(input("Enter a learning rate value (alpha): "))
+
+    for num in data:
+        x.append(num[0])
+        y.append(num[1])
+
+    for i in range(10000):
+        for j in range(len(x)):
+            index = np.random.randint(len(x))
+            b0_temp = Beta_0
+            b1_temp = Beta_1
+            y_hat = 1.0 / (1.0 + np.exp(-(b0_temp + b1_temp * x[index])))
+            Beta_0 = b0_temp - alpha * (y_hat - y[index]) * y_hat * (1.0 - y_hat)
+            Beta_1 = b1_temp - alpha * (y_hat - y[index]) * y_hat * (1.0 - y_hat) * x[index]
+
+    b = sorted([random() for i in range(100)])
+    j_b = [Beta_0 + Beta_1 * i for i in b]
+    # Create the plot
+    plt.plot(x, y, 'o')
+    plt.plot(b, j_b, label='Logistic Regression')
+
+    # Add a title
+    plt.title('% of Long Fur vs. % of Dogs in Profile')
+
+    # Add X and y Label
+    plt.xlabel('% Long Fur')
+    plt.ylabel('% Dogs')
+    plt.show()
+
+    x_f = float(input("Percentage of long fur animals in the profile? "))
+
+    y_f = Beta_0 + Beta_1 * x_f
+    perc = y_f * 100.0
+    print("Should show {:.2f}% in feed".format(perc))
+
