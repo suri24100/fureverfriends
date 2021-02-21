d3023ced6f3f5856d8e2c693fdd5cfa824876d2c
aguacoder123 <52142361+aguacoder123@users.noreply.github.com>
2021-02-20 19:10:02 -0500

    db

--- fureverfriends/src/index.js
+++ fureverfriends/src/index.js
@@ -4,8 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import firebase from "firebase";
 
 
+firebase.initializeApp ({
+    apiKey: "AIzaSyCbirB-vp_FHR0dZXE7C9G_wBgew3WbfVg",
+    authDomain: "fureverfriends-66bae.firebaseapp.com",
+    // databaseURl: '';
+    projectId: "fureverfriends-66bae",
+    storageBucket: "fureverfriends-66bae.appspot.com",
+    messagingSenderId: "1029056317935",
+    appId: "1:1029056317935:web:27e3df2335d7074bbd0e1a",
+    measurementId: "G-1XM6V6EB86"
+});
 // BrowserRouter allows anything within App to call Switch/Link/Route objects
 ReactDOM.render(
   <BrowserRouter>
