379e6f61d7911f8b59b449a022c8b2cd5615e201
SarahKettell <sakettell@gmail.com>
2021-02-28 19:35:00 -0500

    Pet Profile: Display error when pet listing not found

--- fureverfriends/src/PetProfile.js
+++ fureverfriends/src/PetProfile.js
@@ -8,7 +8,7 @@ export default function PetProfile(){
     let { name } = useParams();
     let { id } = useParams();
 
-    const [petDetails, setPetDetails] = useState(null);
+    const [petDetails, setPetDetails] = useState();
 
     useEffect(() => {
         if(!petDetails){
@@ -21,7 +21,35 @@ export default function PetProfile(){
     async function getPetData(){
         console.log(id);
         let petData = await getProfileInfo(id);
-        setPetDetails(petData);
+        if(petData){
+            await setPetDetails({
+                petfinder_listing: true,       // check whether from petfinder or not
+                id: petData.id,
+                name: petData.name,
+                photo_url: (petData.photos[0]) ? petData.photos[0].large : "./images/petProfiles/default-placeholder-image.png",
+                profile_url: petData.url,
+                type: petData.type,
+                age: petData.age,
+                breed: petData.breed,
+                location: {
+                    zipcode: petData.contact.address.postcode,
+                    city: petData.contact.address.city,
+                    state: petData.contact.address.state
+                },
+                cared_by: "",
+                gender: petData.gender,
+                fur_length: petData.coat,
+                personality: petData.tags,
+                good_with_pets: "",
+                kid_friendly: "",
+                vaccinated: petData.attributes.shots_current,
+                neutered: petData.attributes.spayed_neutered,
+                bonded_pair: "",
+                allergy_friendly: ""
+            });
+        } else{
+            // pet not found, display error message
+        }
         console.log(petData);
     }
 
@@ -51,19 +79,51 @@ export default function PetProfile(){
     //     allergy_friendly: ""
     // });
 
+    function ProfileContents(){
+        return (
+            <div className="row">
+                <div className="col s12 m3">
+                    <img src={petDetails.photo_url}/>
+                    <h3>Info</h3>
+                </div>
+                <div className="col s12 m9">
+                    <h3>Meet {name}!</h3>
+                    <p></p>
+                    <h3>About Me</h3>
+                    <p></p>
+                    <h3>Contact Information</h3>
+                    <p></p>
+                    <h3>Application Process</h3>
+                    <p></p>
+                </div>
+            </div>
+        )
+    }
+
+    function PageNotFound(){
+        return(
+            <div className="row">
+                <div className="col s12">
+                    <h2>404: Pet Not Found</h2>
+                    <p>Sorry!</p>
+                </div>
+            </div>
+        )
+    }
+
     return(
-        <div>
+        <div className="profile-page">
             <div className="banner-wrap">
                 <Header/>
                 <div className="banner-img-wrap"></div>
-                <div className="heading">
-                    <span>Listing your potential new friend</span>
-                </div>
-                <div className="subheading">
-                    <span>Filter below for better results!</span>
-                </div>
             </div>
-            <h1>You made it to the profile of {name}</h1>
+            <div className="container">
+                {petDetails ?
+                    <ProfileContents />
+                    :
+                    <PageNotFound />
+                }
+            </div>
         </div>
     )
 }
