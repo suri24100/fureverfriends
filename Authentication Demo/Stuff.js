41aee869ad978f14c4a04b7bd822081055ab6749
aguacoder123 <52142361+aguacoder123@users.noreply.github.com>
2021-03-01 00:50:44 -0500

    made a .dir outside the testing environment instead of commenting

---
+++ Authentication Demo/Stuff.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Stuff = ({handleLogOut}) => {
+    return(
+        <section>
+            <nav>
+                <h2> Welcome to login demo</h2>
+                <button onClick={handleLogOut}> logout </button>
+            </nav>
+        </section>
+    )
+}
+
+export default Stuff;
