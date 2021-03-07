107c71fadeecf5a7ef21a3cce41f40c13748a02a
aguacoder123 <52142361+aguacoder123@users.noreply.github.com>
2021-03-06 19:30:08 -0500

    updated

---
+++ fureverfriends/src/fortestingdeletelater.js
@@ -0,0 +1,22 @@
+import firebase from './ffdb/app'
+import 'firebase/firestore'
+import * as ffdb from "@firebase/firestore/dist/exp";
+
+describe("collection('PetInfo')", () => {
+    it("should add data to a collection", () => {
+        const output =
+            ffdb.collection("PetInfo").add({
+                PetName: " ",
+                PetType: " ",
+
+            })
+                .then((docRef) => {
+                    console.log("Document written with ID: ", docRef.id);
+                })
+                .catch((error) => {
+                    console.error("Error adding document: ", error);
+                });
+
+        return output;
+    });
+})
