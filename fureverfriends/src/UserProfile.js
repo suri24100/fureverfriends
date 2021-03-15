a502d5e57c60bcff4466c70b690fc947941a1157
aguacoder123 <52142361+aguacoder123@users.noreply.github.com>
2021-03-15 11:43:29 -0400

    update

--- fureverfriends/src/UserProfile.js
+++ fureverfriends/src/UserProfile.js
@@ -4,87 +4,71 @@ import './css/style.css';
 import './css/home.css';
 import Footer from "./Footer";
 import {Link} from "react-router-dom";
-import {firestore} from "./ffdb";
+import ffdb, {firestore} from "./ffdb";
 import {useAuth} from "./AuthContext";
-import {Alert} from "react-bootstrap";
-import M from "materialize-css";
 
+
+// function deleteVal() {
+//     this.deleteVal = this.delete.bind(this);
+// }
 
 export default function UserProfile() {
-    const {USER, handleSetUSER, setUSER, updateEmail, currentUser} = useAuth();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
-    const [usernameError, setUsernameError] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [username, setUsername] = useState('');
+    const {USER, handleSetUSER, setUSER} = useAuth()
+const [userInfo, setuserInfo] = useState({ //setstate is async
+    userID : " ",//currentUser.uid, //user id should be givennnnnn -- primary key
+    userName : " ",
+    userLocation : 0,  //zip
+    userPhoneNum : " ",
+    userAccountType : " ",
+    userBio : " "
+});
 
-    useEffect(() => {
-        M.AutoInit();
-        if(username === ''){
-            setUsername(USER.username);
-        }
-        console.log("Reloaded");
-    })
 
-    const clearErrors = () => {
-        setError('');
-        setUsernameError('');
-        setEmailError('');
-    }
 
-    function handleInput (e){ //access to input that calls it
-    const id = e.target.id;
-    const value = e.target.value;
-    handleSetUSER(id,value);
-    console.log(USER);
-    }
+function handleinput (props){ //access to input that calls it
+    const id = props.target.id
+    const value = props.target.value //value vs valueOf
+    // console.log(id)
+    // console.log(value)
+    setuserInfo({...userInfo, [id]:value})
+    console.log(userInfo)
+}
 
 //DON'T USE ADD, USE UPDATE - we don't want to add new users, just update them.
-    async function saveUserProfile (e){
-        e.preventDefault();
+    function saveuserprofile (){
+    firestore.collection("UserInfo")
+        .update({  //pass documents from the object
+            userID : " ",
+            userName : " ",
+            userLocation : 0,  //zip
+            userPhoneNum : " ",
+            userAccountType : " ",
+            userBio : " "
+        })//statevariaABLE
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
 
-        const snapshot = await firestore.collection("UserInfo").where("username", "==", USER.username).get();
-        if(!snapshot.empty && USER.username !== username)
-        {
-            clearErrors();
-            setLoading(false);
-            return setUsernameError('Username already taken, please choose another one.');
-        }
+    // deleteVal() {
+    //     CRUD.delete(this.state.userInfo.id)
+    //         .then(() => {
+    //             this.props.refreshList();
+    //         })
+    //         .catch((e) => {
+    //             console.log(e);
+    //         });
+    // }
 
-        try {
-            clearErrors();
-            setLoading(true);
-            if(currentUser.email !== USER.email)
-            {
-                await updateEmail(USER.email);
-            }
-            updateUser();
-            setError("Account was successfully updated!")
-        }catch(err) {
-            clearErrors();
-            setError("Failed to create an account");
-            if (err.code === "auth/email-already-in-use" || err.code === "auth/invalid-email") {
-                setEmailError(err.message);
-            }
-        }
-
-        setLoading(false);
-    }
-
-    function updateUser(){
-         firestore.collection('UserInfo').doc(currentUser.email).update(USER)
-            .then((docRef) => {
-                console.log("Document written with ID: ", docRef);
-            })
-            .catch((error) => {
-                console.error("Error adding document: ", error);
-            });
-    }
 
     return (
         <div className="actionsnav">
 
             <div className="listings-banner-wrap">
+                <Header/>
                 <div className="listings-banner-img-wrap"></div>
             </div>
 
@@ -106,44 +90,39 @@ export default function UserProfile() {
                         <div>
                             <div className="userinfoform">
                                 <form id="userinfo">
-                                    {error && <Alert variant="danger">{error}</Alert>}
-                                    {console.log(username)}
-
                                     <br/>
                                     <div className="input-field col s12">
                                         <input type="text" id="username" name="username"
                                                defaultValue={USER.username}
-                                               onChange={handleInput}/>
+                                               onChange={handleinput}/>
                                         <label htmlFor="username">Enter New Username</label>
-                                        <p className="errorMsg">{usernameError}</p>
                                     </div>
                                     <div className="input-field col s12">
                                         <input type="text" id="first_name" name="first_name"
                                                defaultValue={USER.first_name}
-                                               onChange={handleInput}/>
+                                               onChange={handleinput}/>
                                         <label htmlFor="first_name">Enter Your First Name:</label>
                                     </div>
                                     <div className="input-field col s12">
                                         <input type="text" id="last_name" name="last_name"
                                                defaultValue={USER.last_name}
-                                               onChange={handleInput}/>
+                                               onChange={handleinput}/>
                                         <label htmlFor="last_name">Enter Your Last Name:</label>
                                     </div>
                                     <div className="input-field col s12">
                                         <input type="email" id="email" name="email"
                                                defaultValue={USER.email}
-                                               onChange={handleInput}/>
+                                               onChange={handleinput}/>
                                         <label htmlFor="email">Enter Your Email Address:</label>
-                                        <p className="errorMsg">{emailError}</p>
                                     </div>
                                     <div className="input-field col s12">
                                         <input type="tel" id="phone_number" name="phone_number"
                                                defaultValue={USER.phone_number}
-                                               onChange={handleInput}/>
+                                               onChange={handleinput}/>
                                         <label htmlFor="phone_number">Enter Your Phone Number:</label>
                                     </div>
                                     <div className="input-field col s12">
-                                        <select id="account_type" onChange={handleInput}>
+                                        <select id="account_type" onChange={handleinput}>
                                             <option value="Adopter">Adopter</option>
                                             <option value="Private Owner">Private Owner</option>
                                             <option value="Organization">Organization</option>
@@ -153,21 +132,19 @@ export default function UserProfile() {
                                     <div className="input-field col s12">
                                         <input type="number" id="user_zip" name="user_zip"
                                                defaultValue={USER.user_zip}
-                                               onChange={handleInput}/>
+                                               onChange={handleinput}/>
                                         <label htmlFor="user_zip">Enter Your Zip Code:</label>
                                     </div>
 
                                     <br/>
-                                    <div id="user_bio">
-                                        <label htmlFor="user_bio">About Me: </label>
-                                        <textarea id="user_bio" name="user_message"
-                                                  defaultValue={USER.user_bio}
-                                                  onChange={handleInput}>
-                                        </textarea>
+                                    <div id="userbio">
+                                        <label htmlFor="aboutme">About Me: </label>
+                                        <textarea id="userBio" name="user_message" onChange={handleinput}></textarea>
                                     </div>
                                     <br/>
                                 </form>
-                                <button disabled={loading}  onClick={saveUserProfile}> Save Changes</button>
+                                <button onClick={saveuserprofile}> Save Changes</button>
+                                {/*<button onClick={deleteVal}> Delete Profile</button>*/}
                             </div>
                 </div>
                 </div>
