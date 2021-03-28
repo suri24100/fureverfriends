9028d6d403eedcc666c62db7b5592992c4043a20
SarahKettell <sakettell@gmail.com>
2021-03-28 15:36:01 -0400

    AI Pet Profiles: Assembling the info for a set of ~300 test profiles to be added to Firestore

---
+++ fureverfriends/src/api-modules/AIDataFunctions.js
@@ -0,0 +1,29 @@
+import {test_profile_info} from './test_profiles'
+
+// pick from randomly to assign an account for each profile created
+const TEST_ACCOUNTS = [
+    {email: "sakettell@gmail.com", username: "Skettles"},
+    {email: "dvp5392@gmail.com", username: "dpatel"},
+    {email: "catnaps@fureverfriends.com", username: "CatNaps"},
+    {email: "dogbiscuits@fureverfriends.com", username: "DogBiscuits"},
+    {email: "dogs4ever@fureverfriends.com", username: "Dogs4Ever"},
+    {email: "dvp5392@psu.edu", username: "avocoder123"},
+    {email: "fuzzypetrescue@fureverfriends.com", username: "FuzzyPetRescue"},
+    {email: "jpa5180@psu.edu", username: "jpa5180"},
+    {email: "jpastudillo123@gmail.com", username: "jpa123"},
+    {email: "kittenpaws@fureverfriends.com", username: "KittenPaws"},
+    {email: "lazysundayfurball@fureverfriends.com", username: "LazySundayFurball"},
+    {email: "micercutetoo@fureverfriends.com", username: "MiceRCuteToo"},
+    {email: "petlover123@fureverfriends.com", username: "PetLover123"},
+    {email: "rabbitears34@fureverfriends.com", username: "RabbitEars34"},
+    {email: "riseandmeow@fureverfriends.com", username: "RiseAndMeow"},
+    {email: "surabhi.sahay24@gmail.com", username: "suri24"},
+    {email: "sxk1552@psu.edu", username: "SarahKettell"}
+]
+
+const TEST_INFO = test_profile_info;
+
+
+
+
+
