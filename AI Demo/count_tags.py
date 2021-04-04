fbf041fb4a4376a9b7a5c1d2307e16971db5c67b
jpa5180 <61556479+jpa5180@users.noreply.github.com>
2021-04-03 22:48:49 -0400

    Update count_tags.py

--- AI Demo/count_tags.py
+++ AI Demo/count_tags.py
@@ -13,7 +13,7 @@ tag_count = {}
 for row in sheet1.iter_rows(min_row=3,max_row=sheet1.max_row,min_col=5):
     for cell in row:
         if cell.value != "" and cell.value !=None:
-            tag = cell.value.lower()
+            tag = cell.value.lower().strip()
             tag_count[tag] = tag_count.setdefault(tag, 0) + 1
 
 sorted_tags = dict( sorted(tag_count.items(), key=operator.itemgetter(1),reverse=True))
