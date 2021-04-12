3e0d7abbb8cd41ee6f8a7b4010ef7bd7a9adfd7d
SarahKettell <sakettell@gmail.com>
2021-04-11 20:24:32 -0400

    ErrorPage.js: Done!

--- fureverfriends/src/ErrorPage.js
+++ fureverfriends/src/ErrorPage.js
@@ -1,14 +1,25 @@
 import React, {useEffect, useState, Component} from 'react';
 import './css/404Page.css'
+import {Link} from "react-router-dom";
 
 export default function ErrorPage(){
     return (
         <div className="error-page">
             <div className="signing-banner-wrap">
             </div>
-            <div className="container">
+            <div className="error-contents">
                 <div className="row">
-                    <h1>Error!</h1>
+                    <div className="col s12">
+                        <div className="right message-contents">
+                            <h2>Breaking Mews!</h2>
+                            <p>Unfurtunately, this page does not exist.</p>
+                            <Link className="btn-large" to="/">
+                                <i className="material-icons left">arrow_back</i>
+                                Click to go Home
+                            </Link>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
