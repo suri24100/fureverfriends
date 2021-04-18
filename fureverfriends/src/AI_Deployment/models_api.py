954d14e719c667852a472aaa7eab2f8193b66a3e
JP <jpastudillo123@gmail.com>
2021-04-17 22:29:45 -0400

    Hopefully it finally works, adding AI_Deployment

---
+++ fureverfriends/src/AI_Deployment/models_api.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9adbda76b942c150c9e3c27ff5471a50521b30932ed8e9ee568e0244030262d2
+size 4351
---
+++ fureverfriends/src/AI_Deployment/venv/Lib/site-packages/transformers/models/mbart/convert_mbart_original_checkpoint_to_pytorch.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c555bd323fa377b5ff3089890a04b9d80a24d4218f7fe13abbca6dbc6d612bca
+size 3035
